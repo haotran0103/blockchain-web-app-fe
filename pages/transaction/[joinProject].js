050dd5cd9c2f072eed54211b34bfae49112b8938
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-04-16 13:40:55 +0700

    fix transaction

--- pages/transaction/[joinProject].js
+++ pages/transaction/[joinProject].js
@@ -3,16 +3,16 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
+import axios from "axios";
+import Web3 from "web3";
 export async function getServerSideProps(context) {
   try {
     const id_old = context.query;
     const id = id_old.joinProject;
 
-    console.log(id);
     const postData = await axios.get(
       `http://localhost:8080/apiv1/projects/${id}`
     );
-    console.log(postData);
     const post = postData.data;
 
     return {
@@ -30,38 +30,45 @@ export async function getServerSideProps(context) {
 
 const conductTransaction = ({ post }) => {
   const [accountAddress, setAccountAddress] = useState("");
-  const [amount, setamount] = useState("");
+  const [amount, setamount] = useState(0);
   const [noiDung, setnoiDung] = useState("");
+  useEffect(() => {
+    async function getAccountAddress() {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          // Request account access if needed
+          await window.ethereum.enable();
+          // Get first account address
+          const accounts = await web3.eth.getAccounts();
+          setAccountAddress(accounts[0]);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+    getAccountAddress();
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      const projectData = {
+        amount: amount,
+        toAddress: post.maVi,
+        fromAddress: accountAddress,
+      };
+      console.log(projectData);
       const res = await axios.post(
         "http://localhost:8080/apiv1/transactionCtrl",
         projectData
       );
-      const projectData = {
-        amount: amount,
-      };
       const data = await res.json();
       window.location.href = data.successUrl; // redirect to payment gateway URL
     } catch (error) {
       console.error(error);
     }
   };
-  const getAccountAddress = async () => {
-    if (typeof window !== "object") {
-      // xử lý lỗi hoặc thực hiện các hành động khác
-      return;
-    }
 
-    const web3 = new Web3(window.ethereum);
-    const accounts = await web3.eth.getAccounts();
-    if (accounts.length > 0) {
-      setAccountAddress(accounts[0]);
-    }
-  };
-
-  getAccountAddress();
   return (
     <>
       <section
@@ -87,47 +94,7 @@ const conductTransaction = ({ post }) => {
                   id="amount"
                   placeholder="Nhập vào số lượng token muốn chuyển"
                   type="number"
-                  value={amount}
-                  onChange={(e) => setamount(e.target.value)}
-                  className="form-control"
-                  style={{ maxWidth: "400px", margin: "auto" }}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="description"></label>
-                <input
-                  id="description"
-                  placeholder="Địa chỉ ví muốn gửi"
-                  type="text"
-                  value={post.maVi}
-                  className="form-control"
-                  style={{ maxWidth: "400px", margin: "auto" }}
-                />
-              </div>
-              <div className="form-group text-center">
-                <label htmlFor="submita"></label>
-                <button
-                  style={{ marginTop: 10 }}
-                  type="submit"
-                  className="btn btn-primary"
-                >
-                  Gửi
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-6 offset-lg-3">
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <input
-                  id="amount"
-                  placeholder="Nhập vào số lượng token muốn chuyển"
-                  type="number"
-                  value={amount}
+                  value={amount ? amount : ""}
                   onChange={(e) => setamount(e.target.value)}
                   className="form-control"
                   style={{ maxWidth: "400px", margin: "auto" }}
@@ -145,13 +112,12 @@ const conductTransaction = ({ post }) => {
                 />
               </div>
               <div className="form-group text-center">
-                <label htmlFor="submita"></label>
                 <button
                   style={{ marginTop: 10 }}
                   type="submit"
                   className="btn btn-primary"
                 >
-                  Gửi
+                  hoàn thành thanh toán
                 </button>
               </div>
             </form>
